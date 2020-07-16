8df913107a6f05066598f41ae69302e5c5395d7a
dfealves <daniloferreira.alves@outlook.com>
2020-07-16 10:48:24 -0300

    init skeleton-load

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { ProductService } from '../product.service';
+import { trigger } from '@angular/animations';
+
+import { fadeIn } from '../shared/utils/fadeIn';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
+  // animations: [trigger('fadeIn', fadeIn())],
 })
 export class HomeComponent implements OnInit {
   products = [];
+  dataLoaded: boolean = false;
+  @Input() skeletonLoad;
 
   constructor(private productService: ProductService) {}
 
@@ -16,5 +22,15 @@ export class HomeComponent implements OnInit {
       console.log(data);
       this.products = data;
     });
+  }
+
+  hideSkeletonLoad(hideElement: boolean) {
+    if (hideElement) {
+      this.dataLoaded = true;
+      this.skeletonLoad = false;
+    } else {
+      this.dataLoaded = false;
+      this.skeletonLoad = true;
+    }
   }
 }
